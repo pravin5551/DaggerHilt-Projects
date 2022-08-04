1433ca71ece8e332a5424167a7aefb256fbda4e0
Pravin5551 <pravinvpawar5551@gmail.com>
2022-08-04 12:31:22 +0530

    Model Class added

---
+++ app/src/main/java/com/example/nobrokerdaggerhilt/modelClass/UserUIModel.kt
@@ -0,0 +1,9 @@
+package com.example.nobrokerdaggerhilt.modelClass
+
+//sealed class which returns the data class depending upon the success/failure
+sealed class UserUIModel {
+
+    data class Success(val responseClass: List<ResponseClass>) : UserUIModel()
+
+    data class Failure(val error: String) : UserUIModel()
+}
