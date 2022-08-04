8c214a531856292674393c1a45a563b2b22cc28d
Pravin5551 <pravinvpawar5551@gmail.com>
2022-08-04 12:22:17 +0530

    Project created

---
+++ app/src/main/java/com/example/nobrokerdaggerhilt/modelClass/ResponseClass.kt
@@ -0,0 +1,19 @@
+package com.example.nobrokerdaggerhilt.modelClass
+
+import javax.annotation.Generated
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+//model class for API Json response
+@Generated("com.robohorse.robopojogenerator")
+data class ResponseClass(
+
+    @field:SerializedName("image")
+    val image: String? = null,
+
+    @field:SerializedName("subTitle")
+    val subTitle: String? = null,
+
+    @field:SerializedName("title")
+    val title: String? = null
+)
